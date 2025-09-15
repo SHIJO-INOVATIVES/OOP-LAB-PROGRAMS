e5c7eba92e244d121e357b20b0bf0d35dbd3ee82
SHIJO-INOVATIVES <jibinshijo11@gmail.com>
2025-09-15 22:20:23 +0530

    Create 1c. SelectionSortExample.java

---
+++ 1c. SelectionSortExample.java
@@ -0,0 +1,36 @@
+public class SelectionSortExample
+{
+public static void selectionsort(int[] arr)
+{
+for(int i=0;i<arr.length-1;i++)
+{
+int index=i;
+for(int j=i+1;j<arr.length;j++)
+{
+if(arr[j]<arr[index])
+{
+index=j;
+}
+}
+int Smaller_Number=arr[index];
+arr[index]=arr[i];
+arr[i]=Smaller_Number;
+}
+}
+public static void main(String a[])
+{
+int[] arr1={9,14,3,2,43,11,58,22};
+System.out.println("Before Selection Sort:");
+for(int i:arr1)
+{
+System.out.print(i+" ");
+}
+System.out.println();
+selectionsort(arr1);
+System.out.println("After Selection Sort:");
+for(int i:arr1)
+{
+System.out.print(i+" ");
+}
+}
+}
